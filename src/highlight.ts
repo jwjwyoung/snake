23253ca382f29cd104096dc701da79b40034fb79
jw <x1022069@gmail.com>
2021-04-16 21:48:58 -0500

    only hover on the highlighted code

--- src/highlight.ts
+++ src/highlight.ts
@@ -15,16 +15,19 @@ export function registerHighlightProvider(ctx: ExtensionContext, documentSelecto
         return results;
 		}
 	}));
-    ctx.subscriptions.push(vscode.languages.registerHoverProvider('ruby', {
+    ctx.subscriptions.push(vscode.languages.registerHoverProvider('*', {
         provideHover(document, position, token) {
             for(let i = 0 ; i < fs.length; i ++){
                 let f = fs[i]
                 for(let j = 0; j < f.issues.length; j ++){
                 let p = new vscode.Range(f.issues[j].position.start.line, f.issues[j].position.start.column, f.issues[j].position.end.line, f.issues[j].position.end.column)
-                if (position >= p.start && position <=p.end)
-                    return {
-                        contents: [f.issues[j].reason]
-                    };
+                
+                if ((position.line == p.start.line && position.character >= p.start.character) || position.line > p.start.line )
+                    if ((position.line == p.end.line && position.character <= p.end.character) || position.line < p.end.line ){
+                        return {
+                            contents: [f.issues[j].reason]
+                        };
+                    }
                 }
             }
         }
