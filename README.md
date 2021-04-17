8801522edd9fb380f6731853c4fc44d7388e865e
jw <x1022069@gmail.com>
2021-04-16 22:29:49 -0500

    add folders

--- README.md
+++ README.md
@@ -4,17 +4,11 @@ This is the README for your extension "snake". After writing up a brief descript
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+Highlight buggy application code after schema change. 
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+create the output.json under the application's folder
 
 ## Extension Settings
 
--- src/highlight.ts
+++ src/highlight.ts
@@ -9,10 +9,12 @@ export function registerHighlightProvider(ctx: ExtensionContext, documentSelecto
             let results:vscode.DocumentHighlight[] = []
             for(let i = 0 ; i < fs.length; i ++){
                 let f = fs[i]
-                for(let j = 0; j < f.issues.length; j ++){
-                    let p = new vscode.Range(f.issues[j].position.start.line, f.issues[j].position.start.column, f.issues[j].position.end.line, f.issues[j].position.end.column)
-                    let docHigh = new vscode.DocumentHighlight(p, vscode.DocumentHighlightKind.Read)
-                    results.push(docHigh)
+                if(doc.uri.toString().includes(f.file)){
+                    for(let j = 0; j < f.issues.length; j ++){
+                        let p = new vscode.Range(f.issues[j].position.start.line, f.issues[j].position.start.column, f.issues[j].position.end.line, f.issues[j].position.end.column)
+                        let docHigh = new vscode.DocumentHighlight(p, vscode.DocumentHighlightKind.Read)
+                        results.push(docHigh)
+                    }
                 }
             }
             console.log("results: " + results.length);
@@ -21,16 +23,18 @@ export function registerHighlightProvider(ctx: ExtensionContext, documentSelecto
 	}));
 
     ctx.subscriptions.push(vscode.languages.registerHoverProvider('*', {
-        provideHover(document, position, token) {
+        provideHover(doc, position, token) {
             for(let i = 0 ; i < fs.length; i ++){
                 let f = fs[i]
                 for(let j = 0; j < f.issues.length; j ++){
                 let p = new vscode.Range(f.issues[j].position.start.line, f.issues[j].position.start.column, f.issues[j].position.end.line, f.issues[j].position.end.column)
-                if ((position.line == p.start.line && position.character >= p.start.character) || position.line > p.start.line )
-                    if ((position.line == p.end.line && position.character <= p.end.character) || position.line < p.end.line ){
-                        return {
-                            contents: [f.issues[j].reason.detailed]
-                        };
+                if(doc.uri.toString().includes(f.file)){
+                    if ((position.line == p.start.line && position.character >= p.start.character) || position.line > p.start.line )
+                        if ((position.line == p.end.line && position.character <= p.end.character) || position.line < p.end.line ){
+                            return {
+                                contents: [f.issues[j].reason.detailed]
+                            };
+                        }
                     }
                 }
             }
@@ -42,6 +46,12 @@ export function registerHighlightProvider(ctx: ExtensionContext, documentSelecto
     let fs: any;
 	if (vscode.window && vscode.window.activeTextEditor) {
         console.log("active window and editor")
-        fs = require("/home/junwen/Research/snake/output.json")
+        let folder = vscode.window.activeTextEditor.document.uri.fsPath;
+        console.log("folder " + folder)
+        if(folder){
+            let fn = folder.split("/app/")[0] + "/output.json"
+            console.log("fn " + fn)
+            fs = require(fn)
+        }
 	}
 }
